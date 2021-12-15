ab52b6f1e2e27b9296a105195919c8641458596c
Катерина Мельникова <katrin.airen@yandex.ru>
2021-12-15 14:22:51 +0800

    Добавьте файлы проекта.

---
+++ lab4/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Posilca> posilcas;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            posilcas = new List<Posilca>();
+            posilcas.Add(new Posilca("Петр", "Иван", "Улан-Удэ", 20000, 20));
+            data.ItemsSource = posilcas;
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            posilcas.Add(new Posilca(inputFioOt.Text, inputFioPo.Text, inputAddress.Text, Convert.ToInt32(inputIndex.Text), Convert.ToInt32(inputWeight.Text)));
+            inputFioOt.Text = "";
+            inputFioPo.Text = "";
+            inputIndex.Text = "";
+            inputAddress.Text = "";
+            inputWeight.Text = "";
+            data.ItemsSource = posilcas;
+            data.Items.Refresh();
+        }
+
+        private void Delet(object sender, RoutedEventArgs e)
+        {
+            Posilca posilca = data.SelectedItem as Posilca;
+            posilcas.Remove(posilca);
+            data.Items.Refresh();
+        }
+
+        private void Sum(object sender, RoutedEventArgs e)
+        {
+            if (data.SelectedItems.Count > 2)
+            {
+                MessageBox.Show("Вы выбрали больше 2х посылок");
+            }
+            else
+            {
+                Posilca posilca1 = data.SelectedItems[0] as Posilca;
+                Posilca posilca2 = data.SelectedItems[1] as Posilca;
+                posilcas.Add(posilca1 + posilca2);
+                data.ItemsSource = posilcas;
+                data.Items.Refresh();
+            }
+        }
+
+        private void Compare(object sender, RoutedEventArgs e)
+        {
+            if (data.SelectedItems.Count > 2)
+            {
+                MessageBox.Show("Вы выбрали больше 2х посылок");
+            }
+            else
+            {
+                Posilca posilca1 = data.SelectedItems[0] as Posilca;
+                Posilca posilca2 = data.SelectedItems[1] as Posilca;
+
+                if (posilca1 > posilca2)
+                {
+                    MessageBox.Show($"посылка с адрресом {posilca1.Address} тяжелее", "Важная информация", MessageBoxButton.OK,  MessageBoxImage.Information);
+                }
+                else
+                {
+                    if (posilca1 < posilca2)
+                    {
+                        MessageBox.Show($"посылка с адрресом {posilca2.Address} тяжелее", "Важная информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"посылки равны по весу", "Важная информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ lab4/Posilca.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Posilca : ICloneable
+    {
+        string fioOt;
+        string fioPo;
+        string address;
+        int index;
+        int weight;
+
+        public Posilca(string fioOt, string fioPo, string address, int index, int weight)
+        {
+            this.FioOt = fioOt;
+            this.FioPo = fioPo;
+            this.Address = address;
+            this.Index = index;
+            this.Weight = weight;
+        }
+
+        public string FioOt { get => fioOt; set => fioOt = value; }
+        public string FioPo { get => fioPo; set => fioPo = value; }
+        public string Address { get => address; set => address = value; }
+        public int Index { get => index; set => index = value; }
+        public int Weight { get => weight; set => weight = value; }
+
+        public object Clone()
+        {
+            return new Posilca(fioOt, fioPo, address, index, weight);
+        }
+
+        public static Posilca operator +(Posilca first, Posilca two)
+        {
+            Posilca sum = (Posilca)first.Clone();
+            sum.Weight += two.Weight;
+            return sum;
+        }
+
+        public static bool operator >(Posilca first, Posilca two)
+        {
+            if(first.Weight > two.Weight)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator <(Posilca first, Posilca two)
+        {
+            if (first.Weight < two.Weight)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
